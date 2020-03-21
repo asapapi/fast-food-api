f4eab964d5d10a3236b7e5227015d7737f27d84b
jimmy-bellinghausen <jimmy.bellinghausen@gmail.com>
2020-03-21 12:32:50 -0500

    init commit

---
+++ src/main/java/com/galvanize/fastfoodapi/FastfoodapiApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.fastfoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastfoodapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastfoodapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/galvanize/fastfoodapi/FastfoodapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.fastfoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastfoodapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
